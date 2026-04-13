d6053252d56f9bf8894d3722deff6bade8ba6dc1
MoMaimon <myomy246@gmail.com>
2026-04-13 17:43:12 +0300

    feat(frontend): Refactor college page to use async data fetching and add Badge component

--- app/colleges/[id]/page.tsx
+++ app/colleges/[id]/page.tsx
@@ -1,25 +1,14 @@
-"use client";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { CourseSchema } from "@/types/db_types";
-import Link from "next/link";
-import { useEffect, useState } from "react";
 
-export default function page({ params }: { params: Promise<{ id: string }> }) {
-  const [data, setData] = useState<CourseSchema[]>();
-  const [id, setId] = useState("2")
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    console.log(params.then(({ id }) => id));
-    params.then(({ id }) => {setId(id)})
-    
-    fetch(`/api/scrape/courses?colleges=${id}`)
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json.data);
-        setIsLoading(false);
-      })
-      .catch((error) => console.error("Error:", error));
-  }, []);
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getCoursesByCollege } from "@/features/scraping/server/db/repository/course_repository";
+import Link from "next/link";
+
+export default async function page({ params }: { params: Promise<{ id: string }> }) {
+ 
+  const {id} = await params
+  const res = getCoursesByCollege(Number(id))
+  const data = await res
   return (<div className="grid gap-5 grid-cols-[repeat(auto-fit,minmax(350px,1fr))] p-5">
       {data?.map((value) => (
         <Link href={`/courses/${value._id}`} key={value._id}>
@@ -28,8 +17,11 @@ export default function page({ params }: { params: Promise<{ id: string }> }) {
           >
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-6">
               <CardTitle className="text-2xl">{value.name}</CardTitle>
-              <span className="text-muted-foreground text-sm">➔</span>
+              <Badge variant={"default"} className="text-md">hours: {value.hours}</Badge>
             </CardHeader>
+            <CardContent>
+              
+            </CardContent>
           </Card>
         </Link>
       ))}
