2d60c1593a9e66cf11adb259a1e0aadcb45f9cd7
M.Maimon <117573257+MoMaimon@users.noreply.github.com>
2026-05-02 04:06:34 +0300

    feat: implement BAUFetchProvider for API data retrieval with retry and randomized user-agent support

--- features/db/providers/BAU/BAUFetchProvider.ts
+++ features/db/providers/BAU/BAUFetchProvider.ts
@@ -7,19 +7,6 @@ import { CollegeApiResponse, CourseApiResponse, DegreeApiResponse, DepartmentApi
 
 export class BAUFetchProvider implements IFetchProvider {
     private readonly baseConfig = config;
-    private readonly DEFAULT_HEADERS = {
-        accept: "*/*",
-        "accept-language": "en-US,en;q=0.9,ar;q=0.8",
-        "content-type": "application/x-www-form-urlencoded",
-        "sec-ch-ua":
-            '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        Referer: "https://app2.bau.edu.jo:7799/courses/index.jsp",
-    };
 
 
     private getRandomUserAgent(): string {
